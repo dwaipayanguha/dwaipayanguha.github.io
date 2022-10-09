1f1a44fda465c346cee2533f91cf084f5df0c982
dwaipayanguha10 <dwaipayanguha10@gmail.com>
2022-10-09 21:44:55 +0530

    Updates

---
+++ precache-manifest.e462547383d317f2882cd7178e68436d.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6407889abc91091e010e262ca68edb68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3de4074495419e71720",
+    "url": "/static/css/3.8565e70b.chunk.css"
+  },
+  {
+    "revision": "f2c1789cea626773e7e9",
+    "url": "/static/css/4.3aab40d2.chunk.css"
+  },
+  {
+    "revision": "96d556dd3bbbb0233134",
+    "url": "/static/css/main.bb071487.chunk.css"
+  },
+  {
+    "revision": "6fd32f9509233a0ad9b7",
+    "url": "/static/js/2.fb4fbf1d.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/static/js/2.fb4fbf1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3de4074495419e71720",
+    "url": "/static/js/3.25399400.chunk.js"
+  },
+  {
+    "revision": "f2c1789cea626773e7e9",
+    "url": "/static/js/4.457826d0.chunk.js"
+  },
+  {
+    "revision": "96d556dd3bbbb0233134",
+    "url": "/static/js/main.ee923b7e.chunk.js"
+  },
+  {
+    "revision": "61f85efcffa5f5e55536",
+    "url": "/static/js/runtime-main.f0e9fdfd.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "31eb36589ec1739d674b4e4b048826ba",
+    "url": "/static/media/aiDriverLogo.31eb3658.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "5c0738221413a976ef659e9c6f6ab305",
+    "url": "/static/media/drowsinessLogo.5c073822.png"
+  },
+  {
+    "revision": "b7165c7fc9f2f34f1aab83b733212c6e",
+    "url": "/static/media/elsevierLogo.b7165c7f.png"
+  },
+  {
+    "revision": "65d7492f587dd3b9a4b59a32d2339b4d",
+    "url": "/static/media/ethereumLogo.65d7492f.png"
+  },
+  {
+    "revision": "4d6a34e922967794c294aed60d0bc7b4",
+    "url": "/static/media/expenseTrackerLogo.4d6a34e9.png"
+  },
+  {
+    "revision": "f3dc306a9e3bb45ec4baef5a53de188b",
+    "url": "/static/media/hackercupLogo.f3dc306a.jpg"
+  },
+  {
+    "revision": "14d32554a8c24f7b09238f9b7abe67d7",
+    "url": "/static/media/iitMadrasLogo.14d32554.png"
+  },
+  {
+    "revision": "2cf71e1159b466860e9ef30ceda8a688",
+    "url": "/static/media/jadavpurUniversityLogo.2cf71e11.png"
+  },
+  {
+    "revision": "4fdc8f5391aa48f75fb8a6bc6a07b378",
+    "url": "/static/media/profilePic.4fdc8f53.jpg"
+  },
+  {
+    "revision": "5883cd4877303420fe18242d1053b010",
+    "url": "/static/media/replyCodeLogo.5883cd48.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "5eaee6112e574456044f1fc8970fea57",
+    "url": "/static/media/wellsFargoLogo.5eaee611.png"
+  }
+]);
